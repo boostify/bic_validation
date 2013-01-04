f03bb4ad24a39d96a6b3c1c3c2d7bdb5717a4424
Frank C. Eckert <frank.eckert@boost-project.com>
2013-01-04 22:59:45 +0100

    first spec

---
+++ spec/bic_validation/bic_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+module BicValidation
+  describe Bic do
+
+    describe '#valid?' do
+      bic = Bic.new "MARKDEFF"
+      bic.valid?.should == true
+    end
+
+  end
+end
