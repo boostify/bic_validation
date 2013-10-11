2d64becab702891735cbfaadf83504474e4ca6ed
Frank C. Eckert <frank.eckert@boost-project.com>
2013-10-11 13:20:24 +0200

    nail code coverage

--- spec/bic_validation/bic_spec.rb
+++ spec/bic_validation/bic_spec.rb
@@ -3,10 +3,28 @@ require 'spec_helper'
 module BicValidation
   describe Bic do
 
+    before { @bic = Bic.new 'MARKDEFF' }
+
+    subject { @bic }
+
     describe '#valid?' do
-      bic = Bic.new 'MARKDEFF'
-      bic.valid?.should == true
+      it { should be_valid }
     end
 
+    describe '#bank' do
+      its(:bank) { should eq('MARK') }
+    end
+
+    describe '#country' do
+      its(:country) { should eq('DE') }
+    end
+
+    describe '#location' do
+      its(:location) { should eq('FF') }
+    end
+
+    describe '#branch' do
+      its(:branch) { should be_nil }
+    end
   end
 end
